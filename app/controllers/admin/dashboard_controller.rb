0a267dd63e1e13b1d95d4d29ee5abab16c5b5a3a
Scott Smyth <ssmythw@outlook.com>
2023-01-25 15:35:31 -0500

    added admin credentials to admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
   def show
+    http_basic_authenticate_or_request_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_or_request_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
   def index
     @products = Product.order(id: :desc).all
   end
