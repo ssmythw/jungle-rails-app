daf7907546cbf9a5c23b1ae524e8e0e870d8a7ff
Scott Smyth <ssmythw@outlook.com>
2023-01-26 11:39:46 -0500

    added categories section to admin panel

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
-  
+
   def show
     @totalProducts = Product.all.count()
     @totalCategories = Category.all.count()
